e8d11e7740f3ad5845554d0c06e138f0bbaa0880
JosuContrer <jdcontrerasalbuj@wpi.edu>
2020-11-11 01:25:24 -0500

    Public Site loading pages tests

--- sigmapiweb/apps/PubSite/test_pubsite.py
+++ sigmapiweb/apps/PubSite/test_pubsite.py
@@ -9,7 +9,7 @@ class PublicViewTests(TestCase):
 
     def test_home_page(self):
         """
-        Test if the home page renders
+        Test if the Home Page renders
         """
         # pub-index here is the name of the url to get the view for, defined in
         # /pubsite/urls.py 
@@ -17,8 +17,47 @@ class PublicViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_brothers_page(self):
+        """
+        Test if Brother Page renders
+        """
         # The view/url/template for the brothers page is actually defined in the
         # UserInfo/ directory. Url name is userinfo-users.
         response = self.client.get(reverse('userinfo-users'))
         print("NUMBER OF BROTHER GROUPS: ", len(response.context['brother_groups']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page(self):
+        """
+        Test if About Page renders
+        """
+        response = self.client.get(reverse('pub-about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_service_page(self):
+        """
+        Test if Service and Activities Page renders with /service path
+        """
+        response = self.client.get(reverse('pub-service'))
+        # Gets a 302 status code that means it is correctly redirecting
+        self.assertEqual(response.status_code, 302)
+
+    def test_activities_page(self):
+        """
+        Test if Service and Activities Page renders with /activities path
+        """
+        response = self.client.get(reverse('pub-activities'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_log_in_page(self):
+        """
+        Test if Log In Page renders
+        """
+        response = self.client.get(reverse('pub-login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_rush_page(self):
+        """
+        Test if Rush Page renders
+        """
+        response = self.client.get(reverse('pub-rush'))
         self.assertEqual(response.status_code, 200)
