1334be51b4be3a4228c745431a59a9536d7f22c1
Jacobvs <jacobvs2@gmail.com>
2021-04-27 21:23:47 -0400

    First test of donor list retrieval

--- sigmapiweb/apps/PubSite/views.py
+++ sigmapiweb/apps/PubSite/views.py
@@ -4,7 +4,7 @@ Views for PubSite app.
 from django.conf import settings
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.shortcuts import render
-
+import requests
 
 def _get_context(page_name):
     return {
@@ -58,6 +58,41 @@ def campaign(request):
     """
     View for the static chapter service page.
     """
+
+    url = 'https://api.givebutter.com/v1/transactions/'
+    headers = {'Authorization': f'Bearer {settings.GIVEBUTTER_API_KEY}'}
+    response = None
+    print(headers)
+    # Make GET request to server, timeout in seconds
+    try:
+        r = requests.get(url, headers=headers, timeout=3, allow_redirects=False)
+        if r.status_code == 301 or r.status_code == 307: # workaround for dropped auth headers due to redirect
+            r = requests.get(r.headers['Location'], headers=headers, allow_redirects=False)
+            if r.status_code == 301 or r.status_code == 307:  # workaround for dropped auth headers due to redirect
+                r = requests.get(r.headers['Location'], headers=headers, allow_redirects=False)
+                if r.status_code == 200:
+                    response = r.json()
+                else:
+                    print(f"ERROR in re-request: {r.status_code}")
+        else:
+            print(f"ERROR in request: {r.status_code}")
+    except requests.exceptions.Timeout:
+        print("Connection Timed out")
+    except requests.ConnectionError:
+        print("Connection to GiveButter API could not be resolved")
+    except requests.exceptions.RequestException:
+        print("An unknown issue occurred while trying to retrieve GiveButter Donor List")
+
+    # Check for successful response, if so - filter, sort, and format data
+    if response and 'data' in response:
+        print(f'Response: {response}')
+        response = response['data']
+        print(f'Data: {response}')
+        successful_txs = [tx for tx in response if tx['status'] == 'success']
+        print(f'Filtered: {successful_txs}')
+        sorted_txs = sorted(successful_txs, key=lambda tx: tx['amount'], reverse=True)
+        print(f'Sorted: {sorted_txs}')
+
     return render(
         request,
         'public/campaign.html',
